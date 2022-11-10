dc9057c66e046b40305559d346fa50bc7ebdfbae
Diaskk <96990855+Diaskk@users.noreply.github.com>
2022-11-10 22:56:18 +0600

    Delete src directory

--- src/website/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template, request
-from flask_login import login_required,  current_user
-routes = Blueprint('routes', __name__)
-
-
-@routes.route('/', methods=['GET', 'POST'])
-@login_required
-def home():
-    return render_template("home.html", user=current_user)
